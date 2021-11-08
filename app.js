4c96090c8fcc5ae6554d95fb7681d470367caebd
Shahriar Mohsin Shohan <79266735+blackcymbal@users.noreply.github.com>
2021-11-08 21:53:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -44,8 +44,9 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err);
 })
 
-//Server
-app.listen(3000, ()=>{
+// Production
 
-    console.log('server is running http://localhost:3000');
+var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port)
 })
